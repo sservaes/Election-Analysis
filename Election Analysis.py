539b889e6fa982dfc949c3e691a1137d397f03e2
Stijn <stijnservaes19@gmail.com>
2020-03-17 00:24:29 +0100

    Imported necessary modules

---
+++ Election Analysis.py
@@ -0,0 +1,10 @@
+# %% Import modules
+
+import pandas as pd
+import numpy as np
+from pandas import Series, DataFrame
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('whitegrid')
+
+# %%
